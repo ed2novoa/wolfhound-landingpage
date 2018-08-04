b970b78218fc3eed2f5f33a438d7c137bae1fc19
Eddie Novoa <eddie@costaricasoftwareservices.com>
2018-08-04 17:12:01 -0600

    Make contact form to actually send messages

--- scripts/custom-scripts.js
+++ scripts/custom-scripts.js
@@ -5,21 +5,21 @@ var scroll = new SmoothScroll('a[href*="#"]', {easing: 'easeInOutCubic', offset:
 
 /****** FORM SCRIPTS *****/
 
-$("#contactForm").submit(function(b) {
-  var a = "";
+$("#contactForm").submit(function(e) {
+  var message = "";
   if ($("#name").val() == "") {
-    a += "Favor ingrese un Nombre.<br>"
+    message += "Favor ingrese un Nombre.<br>"
   }
   if ($("#email").val() == "") {
-    a += "Favor ingrese su Email<br>"
+    message += "Favor ingrese su Email<br>"
   }
   if ($("#subject").val() == "") {
-    a += "Favor ingrese un Asunto.<br>"
+    message += "Favor ingrese un Asunto.<br>"
   }
   if ($("#content").val() == "") {
-    a += "Favor ingrese su mensaje o consulta.<br>"
+    message += "Favor ingrese su mensaje o consulta.<br>"
   }
-  if (a != "") {
+  if (message != "") {
     $("#resultDiv").html('<div class="alert alert-danger" role="alert">' + a + "</div>");
     return false
   } else {
@@ -32,16 +32,16 @@ function submitForm() {
     type: "POST",
     url: "http://novoaweb.com/test/wolfhound/submitForm.php",
     data: $("#contactForm").serialize(),
-    success: function(a) {
-      setTimeout(c(a), 1000);
-      function c(e) {
+    success: function(r) {
+      setTimeout(showResponse(r), 1000);
+      function showResponse(e) {
+        $("#resultDiv").removeClass("opacity-0")
         if (e == 1) {
           $("#resultDiv").html('<div class="alert alert-success" role="alert">¡Éxito! Muchas gracias.<br>Nos estaremos comunicando con usted dentro de poco.</div>');
         } else {
           $("#resultDiv").html('<div class="alert alert-danger" role="alert"><p><strong>Su mensaje no pudo ser enviado.<br>Por favor inténtelo de nuevo.</div>')
         }
       }
-      d.to(b, 0.6, {opacity: 1})
     },
     complete: function() {
       $("#loadingIcon").hide()
